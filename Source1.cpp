2ed0f799c46ee5275dfe119c5781cac060dfa14b
hazard00 <42498911+hazard00@users.noreply.github.com>
2020-05-09 10:15:06 -0700

    Cpp Basics

---
+++ Source1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x, y;
+	cout << "PLEASE ENTER VALUES IN X" << endl;
+	cin >> x;
+	cout << " PLEASE ENTER VALUES IN Y" << endl;
+	cin >> y;
+	cout << "THE MULTIPLICATION IS " << (x * Y);
+	return 0;
+}
