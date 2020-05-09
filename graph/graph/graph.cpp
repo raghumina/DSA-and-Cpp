e780f66868d7d1e439786eb7fadbb3776566f5a7
hazard00 <42498911+hazard00@users.noreply.github.com>
2020-05-09 09:59:48 -0700

    Graph Basics

---
+++ graph/graph/graph.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x ;
+	cout << "ENTER THE VALUES OF X";
+	cin >> x;
+	cout << "the value of x is :" << x << "SUM OF X IS" << (x + x);
+	//cout << "your name is visusl studio" << endl << "hello owrld";
+	return 0;
+}
